dc679cf563d5cdd0b9e7f80cbd7815a40fdf059c
Obsercorma <techrusse@mythologicarte.fr>
2025-06-28 20:27:40 +0200

    Integrate TinyGPS++ library and refactor GPS logic
    Added TinyGPS++ library source files to the project and updated RoadTripGPS.ino to use the new library. Refactored pin assignments, button handling, and LED status logic for improved clarity and reliability. Enhanced error handling and status indication, and improved data logging with more robust checks for GPS and sensor data.

--- RoadTripGPS.ino
+++ RoadTripGPS.ino
@@ -1,45 +1,89 @@
 #include<SoftwareSerial.h>
-#include<TinyGPSPlus.h>
+#include "TinyGPSPlus.h"
 #include<SPI.h>
 #include<SD.h>
 #include<DHT.h>
 
 //#define DEBUG_MODE
 
+/*
+ * PINS
+ * SD: 
+ * MISO: 12
+ * MOSI: 11
+ * CLK: 13
+ * CS: 4
+ * 
+ * LEDs: red:2 ; green:3 ; blue:10
+ * GPS: RX:6 ; TX:5
+ * DHT: 7
+ * BTNs: step:8 ; start/end:9
+ * 
+* STATUS CASES:
+* RED BLINK: Something went wrong
+* PERMANENT RED: GPS Does not receive data properly
+* GREEN BLINK: Ending hike
+* PERMANENT GREEN: Everything is okay
+* BLUE BLINK (2times): Got invalid GPS data
+* BLUE BLINK (4times): Step button pressed
+* BLUE BLINK (1time 1s): Reach button pressed
+*/
+
 #define TIME_INTERVAL 8000
-#define ALERT_PIN 8
 
-void blinkAlert(uint8_t vc);
+void blinkAlert(uint8_t vc, uint16_t d, uint8_t pin, bool sbo=false);
 
-//const uint8_t pausePin = 10;
-const uint8_t reachDestPin = 9;
+const uint8_t reachBtn = 8;
+const uint8_t endBtn = 9;
 const uint8_t dhtPin = 7;
+
+const uint8_t redPin = 2;
+const uint8_t greenPin = 3;
+const uint8_t bluePin = 10;
+
+const uint8_t sdPin = 4;
+
 bool hasResumed = false;
 bool valPause = false;
 bool valReach = false;
-bool stateAlertPin = false;
+bool endBtnTriggered = false;
 uint32_t timeCBtn = 0;
 
 DHT dht(dhtPin, DHT11);
 File fileWrite;
-SoftwareSerial gpsDevice(6, 5); // RX, TX
+SoftwareSerial gpsDevice(6, 5);
 TinyGPSPlus gps;
 String filename = "dat01.txt";
 uint32_t timeCount = 0;
 void setup() {
-  pinMode(ALERT_PIN, OUTPUT);
-  //pinMode(pausePin, INPUT_PULLUP);
-  pinMode(reachDestPin, INPUT_PULLUP);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  
+  pinMode(endBtn, INPUT_PULLUP);
+  pinMode(reachBtn, INPUT_PULLUP);
+  
   #ifdef DEBUG_MODE
   Serial.begin(9600);
   #endif
-  if (!SD.begin(4)) {
-    digitalWrite(ALERT_PIN, HIGH);
+  if (!SD.begin(sdPin)) {
+    digitalWrite(redPin, HIGH);
     #ifdef DEBUG_MODE
     Serial.println("initialization failed!");
     #endif
     while (1){
-      blinkAlert(3);
+      blinkAlert(3, 200, redPin);
+      delay(1000);
+    }
+  }
+  fileWrite = SD.open(filename, FILE_WRITE);
+  if(!fileWrite){
+    #ifdef DEBUG_MODE
+    Serial.println("SD or File not readable!");
+    #endif
+    while(1) 
+    {
+      blinkAlert(3, 200, redPin);
       delay(1000);
     }
   }
@@ -48,46 +92,68 @@ void setup() {
   #ifdef DEBUG_MODE
   Serial.println("Starting");
   #endif
+  blinkAlert(1, 200, redPin);
+  blinkAlert(1, 200, bluePin);
+  blinkAlert(1, 200, greenPin);
 }
 
-void blinkAlert(uint8_t vc){
+void blinkAlert(uint8_t vc, uint16_t d, uint8_t pin, bool sbo=false){
   for(int i=0;i<vc;i++){
-    digitalWrite(ALERT_PIN, LOW);
-    delay(200);
-    digitalWrite(ALERT_PIN, HIGH);
-    delay(200);
+    digitalWrite(pin, LOW);
+    delay(d);
+    digitalWrite(pin, HIGH);
+    delay(d);
   }
-  digitalWrite(ALERT_PIN, stateAlertPin);
+  digitalWrite(pin, sbo);
 }
 
 void loop() {
+  if(endBtnTriggered){
+    blinkAlert(1, 200, greenPin);
+    blinkAlert(1, 200, bluePin);
+    blinkAlert(1, 200, redPin);
+    while(1) delay(1000);
+  }
   delay(25);
-  bool va = digitalRead(reachDestPin);
-  if(va == LOW) {
+  bool vr = digitalRead(reachBtn);
+  bool ve = digitalRead(endBtn);
+  if(ve == LOW){
     timeCBtn = millis();
     do{
-      va = digitalRead(reachDestPin);
+      ve = digitalRead(endBtn);
+      delay(10);
+    }while(ve == LOW);
+    if((millis()-timeCBtn) > 2000){
+      endBtnTriggered = true;
+      fileWrite.println("RT_END");
+      fileWrite.close();
+      
+      #ifdef DEBUG_MODE
+      Serial.println("RT_END");
+      #endif
+      blinkAlert(3, 400, greenPin);
     }
-    while(va == LOW);
-    #ifdef DEBUG_MODE
-    if((millis()-timeCBtn) > 1000){
+  }
+  if(vr == LOW) {
+    timeCBtn = millis();
+    do{
+      vr = digitalRead(reachBtn);
+      delay(10);
+    }
+    while(vr == LOW);
+    if((millis()-timeCBtn) > 1500){
       valReach = true;
-      blinkAlert(4);
+      blinkAlert(1, 1000, bluePin);
+      #ifdef DEBUG_MODE
       Serial.println("VALReach!");
+      #endif
     }else{
-      blinkAlert(2);
+      blinkAlert(4, 300, bluePin);
       valPause = true;
+      #ifdef DEBUG_MODE
       Serial.println("VALPause!");
+      #endif
     }
-    #else
-    if((millis()-timeCBtn) > 1000){
-      valReach = true;
-      blinkAlert(4);
-    }else{
-      blinkAlert(2);
-      valPause = true;
-    }
-    #endif
   }
   while (gpsDevice.available() > 0) {
     gps.encode(gpsDevice.read());
@@ -95,50 +161,51 @@ void loop() {
   if ((millis() - timeCount) > TIME_INTERVAL)
   {
     timeCount = millis();
-    //if(gps.location.isValid()){
-    fileWrite = SD.open(filename, FILE_WRITE);
-    if (fileWrite) {
-      float h = dht.readHumidity();
-      float t = dht.readTemperature();
-      if(valPause){
-        #ifdef DEBUG_MODE
-        Serial.println(hasResumed ? "RT_RESUMED" : "RT_PAUSED");
-        #endif
-        fileWrite.println(hasResumed ? "RT_R" : "RT_P");
-        hasResumed = !hasResumed;
-        valPause = false;
-        delay(1000);
-      }
-     if(valReach){
-        fileWrite.println("DEST_R");
-        valReach = false;
-        delay(1000);
-      }
-      if (gps.location.isValid() && gps.location.isUpdated()) {
-        digitalWrite(ALERT_PIN, (stateAlertPin=LOW));
-        fileWrite.print(String(gps.location.lat(), 16));
-        fileWrite.print(";" + String(gps.location.lng(), 16));
-      } else {
-        digitalWrite(ALERT_PIN, (stateAlertPin=HIGH));
-        fileWrite.print("LT_I;LG_I");
-      }
-      fileWrite.print(";" + ((gps.altitude.isValid() && gps.altitude.isUpdated()) ? (String)gps.altitude.meters() : "AT_I"));
-      fileWrite.print(
-        ";" + ((gps.date.isValid() && gps.date.isUpdated()) ?
-               (String)gps.date.day() + "/" +
-               (String)gps.date.month() + "/" +
-               (String)gps.date.year() + "#V"
-               : "00/00/2000#NV"));
-      fileWrite.print(
-        ";" + ((gps.time.isValid() && gps.time.isValid()) ?
-               (String)(gps.time.hour()) + ":" +
-               (String)gps.time.minute() + "#V"
-               : "00:00#NV"));
-      fileWrite.println(";" + (!isnan(h) && !isnan(t) ? (String)dht.computeHeatIndex(t, h, false) + "#V" : "0.0#NV"));
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    if(valPause){
       #ifdef DEBUG_MODE
-      Serial.println("Data written!");
+      Serial.println(hasResumed ? "RT_RESUMED" : "RT_PAUSED");
       #endif
-      fileWrite.close();
+      fileWrite.println(hasResumed ? "RT_R" : "RT_P");
+      hasResumed = !hasResumed;
+      valPause = false;
+      delay(1000);
     }
+   if(valReach){
+      fileWrite.println("DEST_R");
+      valReach = false;
+      delay(1000);
+    }
+    if (gps.location.isValid() && gps.location.isUpdated()) {
+      digitalWrite(greenPin, HIGH);
+      fileWrite.print(String(gps.location.lat(), 16));
+      fileWrite.print(";" + String(gps.location.lng(), 16));
+    } else {
+      blinkAlert(2, 200, bluePin);
+      #ifdef DEBUG_MODE
+      Serial.println("Invalid GPS Data");
+      #endif
+      fileWrite.print("LT_I;LG_I");
+    }
+    fileWrite.print(";" + ((gps.altitude.isValid() && gps.altitude.isUpdated()) ? (String)gps.altitude.meters() : "AT_I"));
+    fileWrite.print(
+      ";" + ((gps.date.isValid() && gps.date.isUpdated()) ?
+             (String)gps.date.day() + "/" +
+             (String)gps.date.month() + "/" +
+             (String)gps.date.year() + "#V"
+             : "00/00/2000#NV"));
+    fileWrite.print(
+      ";" + ((gps.time.isValid() && gps.time.isValid()) ?
+             (String)(gps.time.hour()) + ":" +
+             (String)gps.time.minute() + "#V"
+             : "00:00#NV"));
+    #ifdef DEBUG_MODE
+    if(isnan(h) || isnan(t)) Serial.println("Invalid DHT11 data!");
+    #endif
+    fileWrite.println(";" + (!isnan(h) && !isnan(t) ? (String)dht.computeHeatIndex(t, h, false) + "#V" : "0.0#NV"));
+    #ifdef DEBUG_MODE
+    Serial.println("Data written!");
+    #endif
   }
 }
